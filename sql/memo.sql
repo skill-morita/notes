8f53d5844fddae6162b4ff1f1f5aa6db928cce14
boppan <boppuncle.7102@gmail.com>
2020-02-23 17:37:30 +0900

    修正

--- sql/memo.sql
+++ sql/memo.sql
@@ -1,3 +1,6 @@
+<!--
+{"id":"17680117127196692623","title":"[テスト][DB][Office・VBA]SQLメモ","categories":[],"draft":"no"}
+-->
 ----------------------------
 --列名取得
 ----------------------------
@@ -20,3 +23,26 @@ FROM (
      UNION SELECT aaa3 AS ColA, 'TblC' FROM TblC
 ) Tbl1
 WHERE Tbl1.ColA LIKE '%flg%'
+
+----------------------------
+--既存データをSELECT⇒INSERTをしたい
+----------------------------
+--PostgreSQL
+INSERT INTO tblA(colA, colB)
+SELECT genelate_series(-100, -10) AS colA
+, '0001' AS colB;
+
+----------------------------
+--UPSERT
+----------------------------
+--PostgreSQL 9.5より前
+WITH upsert AS (
+	UPDATE TblA SET ColA = val_ColA
+	WHERE ColKey = val_Key
+	RETURNING val_Key
+)
+INSERT INTO TblA (ColKey, ColA)
+	SELECT val_Key, val_ColA From TblA
+	WHERE NOT EXISTS (
+		SELECT ColKey FROM upsert
+	);
