3fad045e8ef49362a923701aa402e71b6dc79906
boppan <boppuncle.7102@gmail.com>
2020-03-28 17:49:37 +0900

    修正

--- sql/Oracle_EMExpress_ChangeMode.sql
+++ sql/Oracle_EMExpress_ChangeMode.sql
@@ -1,5 +1,15 @@
---sqlplus / as sysdba --ログイン
+# sqlplus / as sysdba --ログイン
 @?/rbms/admin/execemx emx --Flash版に変更
 
---sqlplus / as sysdba --ログイン
+# sqlplus / as sysdba --ログイン
 @?/rbms/admin/execemx omx --JET版に変更
+
+-- > セッションが変更されました。
+-- > レコードが選択されませんでした。
+-- > 旧 1:select nvl('&1','omx') p1 from dual
+-- > 新 1:select nvl('emx','omx') p1 from dual
+-- > P1
+-- > -----
+-- > emx
+-- > PL/SQLプロシージャが正常に完了しました。
+-- > セッションが変更されました。
