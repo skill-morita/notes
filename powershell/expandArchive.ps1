1133d3bb55b05e4af815f0e033a91340b1de398f
boppan <boppuncle.7102@gmail.com>
2020-07-23 18:19:34 +0900

    定数をパラメータ化

--- powershell/expandArchive.ps1
+++ powershell/expandArchive.ps1
@@ -1,15 +1,16 @@
 ﻿# ===========================================
 # zipファイルを展開し、そのフォルダ内にzipを移動する
+#
+# 【使い方】
+# .\expandArchive.ps1 -workpath C:\_git\__worktmp\
 # ===========================================
 # -------------------------------------------
-# 定数
+# パラメータ
 # -------------------------------------------
-# 処理フォルダ
-# $TARGET_DIR = "C:\_git\__worktmp\"
-$TARGET_DIR = "C:\_git\__worktmp\______temp\"
-# 拡張子
-$EXT = ".zip"
-
+Param(
+    [ValidateSet("C:\_git\__worktmp\" , "C:\_git\__worktmp\______temp\")]$workpath, #作業フォルダ
+    [ValidateSet(".zip")]$ext # 拡張子
+)
 # -------------------------------------------
 # 関数
 # -------------------------------------------
@@ -17,21 +18,33 @@ $EXT = ".zip"
 .".\common\module.ps1"
 
 function main {
+param (
+    $workpath_,
+    $ext_
+)
     # 圧縮ファイル一覧取得 再帰なし
-    $zipfile = Get-ChildItem -Path $TARGET_DIR -File -Filter *$EXT
+    $zipfile = Get-ChildItem -Path $workpath_ -File -Filter *$ext_
     # $zipfile
     $zipfile | ForEach-Object {
-        $dirnm = ($_.FullName).Replace($EXT, "")
+        $dirnm = ($_.FullName).Replace($ext_, "")
         # zip展開
+        # TODO zip以外の拡張子
         Expand-Archive -Path $_.FullName -DestinationPath $dirnm 
-        Write-ErrorLog $TARGET_DIR
+        Write-ErrorLog $workpath_
         # 展開したフォルダにzipを移動
         Move-Item $_.FullName $dirnm
-        Write-ErrorLog $TARGET_DIR
+        Write-ErrorLog $workpath_
     }
 }
 
 # -------------------------------------------
 # 実行
 # -------------------------------------------
-main
+# パラメータ指定がない場合
+if ($null -eq $workpath) {
+    $workpath = "C:\_git\__worktmp\" 
+}
+if ($null -eq $ext) {
+    $ext = ".zip"
+}
+main $workpath $ext
